25ca26dc78a8f6ae82c70f37b96472ae49c40b2c
Ultrapomemosha <vapilovartem@gmail.com>
2023-11-30 18:40:36 +0300

    restart

--- frontend/src/stores/tabs.js
+++
@@ -1,8 +0,0 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
-
-export const useTabsStore = defineStore('tabs', () => {
-  const currentTab = ref('');
-
-  return { currentTab };
-})
